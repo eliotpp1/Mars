541fbd62b47484c46603bc038a63fa55ffd91c93
LeoTarpinWeb <135838829+LeoTarpinWeb@users.noreply.github.com>
2025-03-10 23:35:36 +0100

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,33 +7,31 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home";
 import Terre from "./game/page/Terre";
-import Scene2 from "./game/page/Atmosphere";
+import Ciel from "./game/page/Ciel";
 import Scene3 from "./game/page/Space";
 import Lune from "./game/page/Lune";
-import Game from "./game/page/Game"; // Correction ici
 import Vehicles from "./components/Vehicles";
 import Shop from "./components/Shop";
 import EndGame from "./components/EndGame";
-import { SoundProvider } from "./context/SoundContext";
-import SoundOverlay from "./components/SoundOverlay";
+import { SoundProvider } from "./context/SoundContext"; // Importer le contexte
+import SoundOverlay from "./components/SoundOverlay"; // Importer l'overlay
 
 function App() {
   return (
     <SoundProvider>
       <Router>
-        <SoundOverlay /> {/* Correction : Mettre l'overlay avant Routes si nécessaire */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/terre" element={<Terre />} />
-          <Route path="/scene2" element={<Scene2 />} />
+          <Route path="/ciel" element={<Ciel />} />
           <Route path="/scene3" element={<Scene3 />} />
-          <Route path="/game" element={<Game />} /> {/* Correction ici */}
           <Route path="/lune" element={<Lune />} />
           <Route path="/vehicles" element={<Vehicles />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/gameover" element={<EndGame />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+        <SoundOverlay /> {/* Overlay global */}
       </Router>
     </SoundProvider>
   );
