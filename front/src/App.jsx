711ae7a7d9b903f9881fa294248e9555483cace4
LeoTarpinWeb <135838829+LeoTarpinWeb@users.noreply.github.com>
2025-03-11 15:13:54 +0100

    Reapply "début mars"
    This reverts commit 7f7201735e417f3e5491d2b1539285ed7339f547.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,15 +7,17 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home";
 import Terre from "./game/page/Terre";
-import Ciel from "./game/page/Ciel";
+import Ciel from "./game/page/ciel";
 import Scene3 from "./game/page/Space";
 import Lune from "./game/page/Lune";
+import { Mars } from "./game/page/Mars";
 import Vehicles from "./components/Vehicles";
 import Shop from "./components/Shop";
 import EndGame from "./components/EndGame";
 import { SoundProvider } from "./context/SoundContext"; // Importer le contexte
 import { ProgressProvider } from "./context/ProgessContext";
 import SoundOverlay from "./components/SoundOverlay"; // Importer l'overlay
+
 
 function App() {
   return (
@@ -28,6 +30,7 @@ function App() {
             <Route path="/ciel" element={<Ciel />} />
             <Route path="/scene3" element={<Scene3 />} />
             <Route path="/lune" element={<Lune />} />
+            <Route path="/mars" element={<Mars />} />
             <Route path="/vehicles" element={<Vehicles />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/gameover" element={<EndGame />} />
